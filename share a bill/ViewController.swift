0587f9b299d9311ff4c2442a994339bf1419888c
Paulina Matysiak <pmatysiak@paulinas-macbook-pro.local>
2019-03-24 16:09:03 +0100

    Initial Commit

---
+++ share a bill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  share a bill
+//
+//  Created by Paulina Matysiak on 24/03/2019.
+//  Copyright © 2019 Paulina Matysiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ share a billTests/share_a_billTests.swift
@@ -0,0 +1,34 @@
+//
+//  share_a_billTests.swift
+//  share a billTests
+//
+//  Created by Paulina Matysiak on 24/03/2019.
+//  Copyright © 2019 Paulina Matysiak. All rights reserved.
+//
+
+import XCTest
+@testable import share_a_bill
+
+class share_a_billTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
