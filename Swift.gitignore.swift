92c7ea70c920e1723de30b43f26977f3b049b3be
Paulina Matysiak <pmatysiak@Paulinas-MacBook-Pro.local>
2019-08-28 19:16:47 +0200

    removing file

--- Swift.gitignore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Swift.gitignore.swift
-//  share a bill
-//
-//  Created by Paulina Matysiak on 28/08/2019.
-//  Copyright © 2019 Paulina Matysiak. All rights reserved.
-//
-
-import Foundation
